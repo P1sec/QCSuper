8d17367d44e7034acbf5d41db7f813764fb9a6a8
Marin <p1-mmr@users.noreply.github.com>
2024-02-06 15:18:04 +0100

    Use the default contents of the "README.md" file as PyPI long description contents

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 #-*- encoding: Utf-8 -*-
-
+from os.path import dirname, realpath
 from setuptools import setup
+
+SCRIPT_DIR = dirname(realpath(__file__))
+README_PATH = SCRIPT_DIR + '/README.md'
+
+with open(README_PATH) as fd:
+    long_description = fd.read()
 
 setup(name = 'qcsuper',
     version = '2.0.0',
     description = ' QCSuper is a tool communicating with Qualcomm-based phones and modems, allowing to capture raw 2G/3G/4G radio frames, among other things',
     author = 'P1 Security - Marin Moulinier',
     author_email = '',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
     entry_points = {
         'console_scripts': [
             'qcsuper = qcsuper.__main__'
