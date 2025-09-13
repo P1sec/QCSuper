e2e97eafad25ac4be5a9b6353544561d32e6ce6d
Thomas Hebb <tommyhebb@gmail.com>
2025-09-13 12:36:13 -0400

    messages_live: Allow filtering to specific subsystems
    This doesn't currently allow sending a range of subsystem IDs, nor do we
    print the mask of each log message, making the mask piece of the
    argument difficult to determine. But it's a good start.

--- src/qcsuper/main.py
+++ src/qcsuper/main.py
@@ -210,6 +210,18 @@ def main():
         description='To be used along with --messages-live.',
     )
 
+    def msg_filter(string):
+        parts = [int(x, 0) for x in string.split(':')]
+        if len(parts) == 1:
+            # Enable all mask bits
+            return [parts[0], 0xFFFFFFFF]
+        elif len(parts) == 2:
+            return parts
+        else:
+            raise ValueError(
+                'message filter must be in the format SUBSYS[:MASK]'
+            )
+
     messages_options.add_argument(
         '--qdb',
         metavar='QSHRINK_DB',
@@ -222,7 +234,13 @@ def main():
         action='store_true',
         help="Don't highlight argument values in bold even when a TTY is present.",
     )
-    # TODO: subsystem argument?
+    messages_options.add_argument(
+        '--msg-filter',
+        metavar='SUBSYS[:MASK]',
+        type=msg_filter,
+        action='append',
+        help='Enable messages for the given subsystem. Specify multiple times, or omit to show all messages.',
+    )
 
     args = parser.parse_args()
 
@@ -346,7 +364,12 @@ def main():
             diag_input.add_module(DlfDumper(diag_input, args.dlf_dump))
         if args.messages_live:
             diag_input.add_module(
-                MessagePrinter(diag_input, args.qdb or [], not args.no_style)
+                MessagePrinter(
+                    diag_input,
+                    args.qdb or [],
+                    args.msg_filter,
+                    not args.no_style,
+                )
             )
 
     # if args.efs_dump:
