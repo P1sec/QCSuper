7f33597cf58fac60cfe36d9c5992f17132f6c10d
Marin <p1-mmr@users.noreply.github.com>
2026-03-21 15:01:52 +0100

    Prototype argument parser in server_main.py

--- src/qcsuper/server/server_main.py
+++ src/qcsuper/server/server_main.py
@@ -1,1 +1,14 @@
-XX WIP
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+def main():
+    args = ArgumentParser(
+        description = 'XX'
+    )
+
+    WIP
+
+    args = args.parse_args()
+
+if __name__ == '__main__':
+    main()
